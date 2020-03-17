07a17071b30f15fe4889c731497ef9b5d60369c8
AustinCapaviella <amcapaviella@gmail.com>
2020-03-16 23:53:01 -0700

    Complete API GET Request

--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
+const db = require('../db/index.js')
+const bodyParser = require('body-parser')
 
+//***************MIDDLEWARE**************\\
+
+app.use(bodyParser())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//****************API CALLS**************\\
 
+//GET request for photos from a given index
+app.get('/photos', (req, res) => {
+  console.log(req.body)
+  db.getListing(req.body.propId, (err, results) => {
+    if (err) {
+      res.send(400);
+    } else {
+      console.log("WHATS REALLY GOOD")
+      res.send(results);
+    }
+});
+});
+
+
+//****************PORT OPENING**************\\
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
