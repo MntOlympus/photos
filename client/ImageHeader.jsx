cf85a743804bbbedbc9f45da715bd6fc32e45f9e
AustinCapaviella <amcapaviella@gmail.com>
2020-03-19 03:13:22 -0700

    Create ImageHeader and ImageCarousel Components
    Create outline for each component with static data
    Add transition between both components on both image click
    and close button click

--- client/ImageHeader.jsx
+++ client/ImageHeader.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const ImageHeader = (props) => {
+
+  const handleClick = (event) => {
+    props.changeView(props.view, event.target.src)
+
+  }
+
   return (<div class="container">
-              <img class ="cell cell-1" width="500" height="500" src="https://mtnolympusimages.s3-us-west-1.amazonaws.com/1/1.jpeg"></img>
+              <img onClick ={handleClick}class ="cell cell-1" width="500" height="500" src={"https://mtnolympusimages.s3-us-west-1.amazonaws.com/1/1.jpeg"}></img>
               {/* <div class="cell cell-1">1.</div> */}
-              <img class ="cell cell-2" src="https://mtnolympusimages.s3-us-west-1.amazonaws.com/1/7.jpeg"></img>
+              <img onClick ={handleClick} class class ="cell cell-2" src={"https://mtnolympusimages.s3-us-west-1.amazonaws.com/1/2.jpeg"}></img>
               {/* <div class="cell cell-2">2.</div> */}
-              <img class ="cell cell-3" src="https://mtnolympusimages.s3-us-west-1.amazonaws.com/1/3.jpeg"></img>
+              <img onClick ={handleClick}class class ="cell cell-3" src={"https://mtnolympusimages.s3-us-west-1.amazonaws.com/1/3.jpeg"}></img>
               {/* <div class="cell cell-3">3.</div> */}
-              <img class ="cell cell-4" src="https://mtnolympusimages.s3-us-west-1.amazonaws.com/1/4.jpeg"></img>
+              <img onClick ={handleClick}class class ="cell cell-4" src={"https://mtnolympusimages.s3-us-west-1.amazonaws.com/1/4.jpeg"}></img>
               {/* <div class="cell cell-4">4.</div> */}
-              <img class ="cell cell-5" src="https://mtnolympusimages.s3-us-west-1.amazonaws.com/1/5.jpeg"></img>
+              <img onClick ={handleClick}class class ="cell cell-5" src={"https://mtnolympusimages.s3-us-west-1.amazonaws.com/1/5.jpeg"}></img>
               {/* <div class="cell cell-5">5.</div> */}
-              <button>hello</button>
           </div>)
 }
 
