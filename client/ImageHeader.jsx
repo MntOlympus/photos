a0bd99d3c6ce4566923d6f852cfca76b321c98fe
AustinCapaviella <amcapaviella@gmail.com>
2020-03-25 18:46:23 -0700

    Refactor Image Header Component for Proxy Server

--- client/ImageHeader.jsx
+++ client/ImageHeader.jsx
@@ -8,18 +8,24 @@ const ImageHeader = (props) => {
     props.changeView(props.view, event.target.id)
   }
 
-  return (<div >
+  return (
             <div className={styles.container}>
-                <img id={0} onClick ={handleClick} className={styles.cell1} width="500" height="500" src={props.images[0].url}></img>
+                <img id={0} onClick ={handleClick} className={styles.cell1} src={props.images[0].url}></img>
                 <img id={1} onClick ={handleClick} className={styles.cell2} src={props.images[1].url}></img>
                 <img id={2} onClick ={handleClick} className={styles.cell3} src={props.images[2].url}></img>
                 <img id={3} onClick ={handleClick} className={styles.cell4} src={props.images[3].url}></img>
                 <img id={4} onClick ={handleClick} className={styles.cell5} src={props.images[4].url}></img>
+                <div className={styles.overlap}>
+                  <button className={styles.showAll} id={0} onClick={handleClick} >Show all photos</button>
+                </div>
+
+                <div>
+
+                </div>
+
             </div>
-              <span>
-                <button className={styles.showAll} id={0} onClick={handleClick} >Show all photos</button>
-              </span>
-          </div>)
+
+  )
 }
 
 export default ImageHeader
