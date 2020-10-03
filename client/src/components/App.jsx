e83ab147bf873e3e9beca717ce8c6661e23c9c4d
AustinCapaviella <amcapaviella@gmail.com>
2020-10-03 16:27:23 -0700

    delay rendering of App compoennt until data is fetched

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import axios from "axios";
 import ImageHeader from "./ImageHeader.jsx";
 import ImageCarousel from "./ImageCarousel.jsx";
@@ -8,34 +8,37 @@ function App() {
   const [images, setImages] = useState([]);
   const [clickedImage, setClickedImage] = useState(0);
   const [view, setView] = useState(true);
+  const [isFetching, setFetching] = useState(true);
 
+  //changes view between ImageHeader <-> ImageCarousel
   const changeView = () => {
     setView(!view);
   };
 
-  // useEffect(async () => {
-  //   const results = await axios("/photos?propId=1");
-  //   setImages(results.data);
-  // }, []);
-
+  //API request to database for image URLs of an individual property
   useEffect(() => {
     async function fetchData() {
-      const results = await axios("/photos?propId=1");
-      setImages(results);
+      const results = await axios("/photos?propId=1").then((results) => {
+        setImages(results);
+        setFetching(false);
+      });
     }
     fetchData();
   }, []);
 
-  if (view === true) {
+  //conditionally renders ImageHeader or ImageCarousel depending on view
+  if (view === true && isFetching === false) {
     let component = (
       <ImageHeader images={images} view={view} changeView={changeView} />
     );
     return <div>{component}</div>;
-  } else {
+  } else if (view === false && isFetching === false) {
     let component = (
       <ImageCarousel images={images} view={view} changeView={changeView} />
     );
     return <div>{component}</div>;
+  } else {
+    return <div>Loading your home.....</div>;
   }
 }
 
--- client/src/components/ImageHeader.jsx
+++ client/src/components/ImageHeader.jsx
@@ -1,31 +1,45 @@
-import React from "react";
+import React, {
+  useState,
+  useEffect,
+  useLayoutEffect,
+  Component,
+  LoadingSpinner,
+} from "react";
 import ReactDOM from "react-dom";
 import styles from "../../styles/ImageHeader.css";
 
 const ImageHeader = (props) => {
-  return (
-    <div>
-      <button onClick={() => props.changeView(props.view)}>
-        hello from IH
-      </button>
-    </div>
+  console.log(props.images.data.length > 1);
 
-    // <div className={styles.container}>
-    //     <img id={0} onClick ={handleClick} className={styles.cell1} src={props.images[0].url}></img>
-    //     <img id={1} onClick ={handleClick} className={styles.cell2} src={props.images[1].url}></img>
-    //     <img id={2} onClick ={handleClick} className={styles.cell3} src={props.images[2].url}></img>
-    //     <img id={3} onClick ={handleClick} className={styles.cell4} src={props.images[3].url}></img>
-    //     <img id={4} onClick ={handleClick} className={styles.cell5} src={props.images[4].url}></img>
-    //     <div className={styles.overlap}>
-    //       <button className={styles.showAll} id={0} onClick={handleClick} >Show all photos</button>
-    //     </div>
-
-    //     <div>
-
-    //     </div>
-
-    // </div>
-  );
+  if (props.images.data.length > 1) {
+    console.log("we are in the end game");
+    return (
+      <div>
+        <button onClick={() => props.changeView(props.view)}>
+          <img src={props.images.data[0].url}></img>
+        </button>
+      </div>
+    );
+  } else {
+    return <div>hi</div>;
+  }
 };
+// }
+
+// <div className={styles.container}>
+//     <img id={0} onClick ={handleClick} className={styles.cell1} src={props.images[0].url}></img>
+//     <img id={1} onClick ={handleClick} className={styles.cell2} src={props.images[1].url}></img>
+//     <img id={2} onClick ={handleClick} className={styles.cell3} src={props.images[2].url}></img>
+//     <img id={3} onClick ={handleClick} className={styles.cell4} src={props.images[3].url}></img>
+//     <img id={4} onClick ={handleClick} className={styles.cell5} src={props.images[4].url}></img>
+//     <div className={styles.overlap}>
+//       <button className={styles.showAll} id={0} onClick={handleClick} >Show all photos</button>
+//     </div>
+
+//     <div>
+
+//     </div>
+
+// </div>
 
 export default ImageHeader;
