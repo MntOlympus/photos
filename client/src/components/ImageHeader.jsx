80e72531bffa59969b71ccfc980bb99732c1851f
AustinCapaviella <amcapaviella@gmail.com>
2020-10-03 23:31:17 -0700

    add custome hook to track browser size

--- client/src/components/ImageHeader.jsx
+++ client/src/components/ImageHeader.jsx
@@ -9,37 +9,76 @@ import ReactDOM from "react-dom";
 import styles from "../../styles/ImageHeader.css";
 
 const ImageHeader = (props) => {
-  console.log(props.images.data.length > 1);
 
-  if (props.images.data.length > 1) {
-    console.log("we are in the end game");
-    return (
-      <div>
-        <button onClick={() => props.changeView(props.view)}>
-          <img src={props.images.data[0].url}></img>
+  const useWindowSize = () => {
+
+    const [windowSize, setWindowSize] = useState({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+
+    useLayoutEffect(() => {
+      const handleResize = () => {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight
+        });
+      };
+
+
+      window.addEventListener("resize", handleResize);
+
+      handleResize();
+
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+    console.log(windowSize)
+    return windowSize;
+  };
+
+  useWindowSize();
+
+  return (
+    <div>
+      <div className={styles.container}>
+        <img
+          id={0}
+          onClick={props.changeView}
+          className={styles.cell1}
+          src={props.images.data[0].url}
+        ></img>
+        <img
+          id={1}
+          onClick={props.changeView}
+          className={styles.cell2}
+          src={props.images.data[1].url}
+        ></img>
+        <img
+          id={2}
+          onClick={props.changeView}
+          className={styles.cell3}
+          src={props.images.data[2].url}
+        ></img>
+        <img
+          id={3}
+          onClick={props.changeView}
+          className={styles.cell4}
+          src={props.images.data[3].url}
+        ></img>
+        <img
+          id={4}
+          onClick={props.changeView}
+          className={styles.cell5}
+          src={props.images.data[4].url}
+        ></img>
+      </div>
+      <div className={styles.overlap}>
+        <button className={styles.showAll} id={0} onClick={props.changeView}>
+          Show all photos
         </button>
       </div>
-    );
-  } else {
-    return <div>hi</div>;
-  }
+    </div>
+  );
 };
-// }
-
-// <div className={styles.container}>
-//     <img id={0} onClick ={handleClick} className={styles.cell1} src={props.images[0].url}></img>
-//     <img id={1} onClick ={handleClick} className={styles.cell2} src={props.images[1].url}></img>
-//     <img id={2} onClick ={handleClick} className={styles.cell3} src={props.images[2].url}></img>
-//     <img id={3} onClick ={handleClick} className={styles.cell4} src={props.images[3].url}></img>
-//     <img id={4} onClick ={handleClick} className={styles.cell5} src={props.images[4].url}></img>
-//     <div className={styles.overlap}>
-//       <button className={styles.showAll} id={0} onClick={handleClick} >Show all photos</button>
-//     </div>
-
-//     <div>
-
-//     </div>
-
-// </div>
 
 export default ImageHeader;
