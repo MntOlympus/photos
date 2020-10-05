58833534ba11b777d7e09b9757d9ba75179bc985
AustinCapaviella <amcapaviella@gmail.com>
2020-10-05 15:45:05 -0700

    remove placeholder single image

--- client/src/components/SingleImage.jsx
+++ client/src/components/SingleImage.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import styles from "../../styles/ImageHeader.css";
+import styles from "../../styles/SingleImage.css";
 
 const SingleImage = (props) => {
-  return (<img
-    id={0}
-    onClick={props.changeView}
-    className={styles.cell1}
-    src={props.images.data[0].url}
-  ></img>
+  return (
+    <div>
+      <img id={0} onClick={props.changeView} className={styles.img} src={props.images.data[0].url}></img>
+    </div>
   )
 }
 
