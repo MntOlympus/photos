0d1e6ddf0b2d812e76d75528b6976016286d8a0d
AustinCapaviella <amcapaviella@gmail.com>
2020-10-21 10:23:40 -0700

    add nav bar components and directory

---
+++ client/src/components/NavBar/ImageCarouselNavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../../../styles/ImageCarouselNavBar.css'
+
+const ImageCarouselNavBar = () => {
+  return (
+    <div>Nav Bar Place Holder</div>
+
+  )
+}
+
+export default ImageCarouselNavBar
